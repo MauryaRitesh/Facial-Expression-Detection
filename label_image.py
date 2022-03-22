d97eaee2cb933dacfc4ad395214dd8a14a6545e7
Ritesh Maurya <31176772+MauryaRitesh@users.noreply.github.com>
2022-03-22 19:09:42 +0530

    Update label_image.py

--- label_image.py
+++ label_image.py
@@ -1,3 +1,4 @@
+## Credit: Some parts of the program has been taken from OpenCV documentation
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function

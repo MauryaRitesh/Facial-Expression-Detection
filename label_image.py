ab5dc07b7df98c63b5f0d368a792c35126c86d2a
smty2018 <74114936+smty2018@users.noreply.github.com>
2023-10-15 16:19:55 +0530

    Update label_image.py

--- label_image.py
+++ label_image.py
@@ -1,4 +1,5 @@
 ## Credit: Some parts of the program has been taken from OpenCV documentation
+#importing required libraries
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -10,6 +11,7 @@ import time
 import numpy as np
 import tensorflow as tf
 
+#function to load TensorFlow graph from a model file
 def load_graph(model_file):
   graph = tf.Graph()
   graph_def = tf.GraphDef()
@@ -21,6 +23,7 @@ def load_graph(model_file):
 
   return graph
 
+#function to read and pre-process the image
 def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
 				input_mean=0, input_std=255):
   input_name = "file_reader"
@@ -46,6 +49,7 @@ def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
 
   return result
 
+#function for loading labels from a file
 def load_labels(label_file):
   label = []
   proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
@@ -53,6 +57,7 @@ def load_labels(label_file):
     label.append(l.rstrip())
   return label
 
+#main function for image classification
 def main(img):
   file_name = img
   model_file = "retrained_graph.pb"
@@ -64,6 +69,7 @@ def main(img):
   input_layer = "input"
   output_layer = "final_result"
 
+  #parsing command-line arguments
   parser = argparse.ArgumentParser()
   parser.add_argument("--image", help="image to be processed")
   parser.add_argument("--graph", help="graph/model to be executed")
@@ -107,7 +113,7 @@ def main(img):
   input_operation = graph.get_operation_by_name(input_name);
   output_operation = graph.get_operation_by_name(output_name);
 
-
+  #running the image through the model
   with tf.Session(graph=graph) as sess:
     start = time.time()
     results = sess.run(output_operation.outputs[0],
@@ -115,6 +121,7 @@ def main(img):
     end=time.time()
   results = np.squeeze(results)
 
+  #identifying the top k results
   top_k = results.argsort()[-5:][::-1]
   labels = load_labels(label_file)
 
